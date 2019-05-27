bf95b1f6c35b31e26bff6c6f043ca0b5b0d66342
Liviu Schera <liviu115@gmail.com>
2019-05-27 13:51:12 +0930

    first commmit

--- src/build.mjs
+++ src/build.mjs
@@ -1,5 +1,6 @@
 import lux from "./workbench/lux.mjs";
 import hibernus from "./workbench/hibernus.mjs";
+import susurrus from "./workbench/susurrus.mjs";
 import noctis from "./workbench/noctis.mjs";
 import azureus from "./workbench/azureus.mjs";
 import obscuro from "./workbench/obscuro.mjs";
@@ -12,6 +13,7 @@ import { buildAllThemes } from "./buildAllThemes.mjs";
 export const THEMES = [
    { lux: lux },
    { hibernus: hibernus },
+   { susurrus: susurrus },
    { noctis: noctis },
    { azureus: azureus },
    { obscuro: obscuro },
